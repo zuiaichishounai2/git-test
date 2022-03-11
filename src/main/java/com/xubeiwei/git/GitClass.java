486eede467a259e416853c9dc9f7d43f914a373a
xubeiwei <xubeiwei@qq.com>
2022-03-11 20:36:56 +0800

    first commit

---
+++ src/main/java/com/xubeiwei/git/GitClass.java
@@ -0,0 +1,11 @@
+package com.xubeiwei.git;
+
+/**
+ * @author xubeiwei
+ *  2022-03-11-20:24
+ */
+public class GitClass {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}

bfa992b8e728ab5479f044ee4df7c46ab70b3cb7
xubeiwei <xubeiwei@qq.com>
2022-03-12 13:24:21 +0800

    gitee commit

--- src/main/java/com/xubeiwei/git/GitClass.java
+++ src/main/java/com/xubeiwei/git/GitClass.java
@@ -14,5 +14,6 @@ public class GitClass {
         System.out.println("hot-fix test");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("gitee test");
     }
 }
